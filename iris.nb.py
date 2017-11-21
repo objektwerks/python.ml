7cb5eeaf92e5511a16db9e7ee6ad7418d59feee3
objektwerks <objektwerks@runbox.com>
2017-11-21 12:28:16 -0500

    enhance debug

--- iris.nb.py
+++ iris.nb.py
@@ -7,7 +7,14 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
 X, y = load_iris(return_X_y=True)
+print("X shape: ", X.shape)
+print("y shape: ", y.shape)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
+print("X train shape: ", X_train.shape)
+print("X test shape: ", X_test.shape)
+print("y train shape: ", y_train.shape)
+print("y test shape: ", y_test.shape)
 
 model = GaussianNB()
 model.fit(X_train, y_train)
