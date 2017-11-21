2bf2021bcb3a775f4b506dd702259e71d74913f2
objektwerks <objektwerks@runbox.com>
2017-11-21 08:09:11 -0500

    refactored iris-dt.py

--- iris-dt.py
+++ iris-dt.py
@@ -3,10 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-
+X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
 
 model = DecisionTreeClassifier()
