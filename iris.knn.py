74d9aecc2876b200b191fcf2bcbe138363e30d32
objektwerks <objektwerks@runbox.com>
2017-11-21 11:36:12 -0500

    added n neighbor to knn classifier

--- iris.knn.py
+++ iris.knn.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
 
-model = KNeighborsClassifier()
+model = KNeighborsClassifier(n_neighbors=5)
 model.fit(X_train, y_train)
 y_predicted = model.predict(X_test)
 
