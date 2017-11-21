6ee4fa73ac1e320c7121ccde597fe1facdaea3e5
objektwerks <objektwerks@runbox.com>
2017-11-21 12:09:08 -0500

    add debug to iris.dtc

--- iris.dtc.py
+++ iris.dtc.py
@@ -7,7 +7,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
 
 X, y = load_iris(return_X_y=True)
+print(X.shape)
+print(y.shape)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=3)
+print(X_train.shape)
+print(X_test.shape)
 
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
