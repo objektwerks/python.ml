614ffbba8e3b5af94863c017fe5804cf9fdc662a
objektwerks <objektwerks@runbox.com>
2017-11-24 12:23:11 -0500

    added sorting to pandas

--- pandas.data.py
+++ pandas.data.py
@@ -8,13 +8,14 @@ print("Ads shape: ", ads.shape)
 print("Ads data:\n", ads.head(n=3))
 
 orders = panda.read_table('./data/orders.tsv', index_col=0)
+orders.drop(['choice_description'], axis=1, inplace=True)
 print("Orders shape: ", orders.shape)
-print("Orders data:\n", orders.head(n=3))
+print("Orders data:\n", orders.sort_values('item_price', ascending=False).head(n=3))
 
 occupation_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
 occupations = panda.read_table('./data/occupations.psv', sep='|', header=None, names=occupation_cols, index_col=0)
 print("Occupations shape: ", occupations.shape)
-print("Occupations data:\n", occupations.head(n=3))
+print("Occupations data:\n", occupations.sort_values('occupation').head(n=3))
 
 ufos = panda.read_csv('./data/ufos.csv')
 assert ufos.City.size > 0
@@ -22,5 +23,7 @@ ufos['Location'] = ufos.City + ', ' + ufos.State
 assert ufos.Location.size > 0
 ufos.rename(columns={'Time': 'Date_Time'}, inplace=True)
 assert ufos.Date_Time.size > 0
+ufos.drop(['Color', 'State'], axis=1, inplace=True)
+ufos.drop([0, 1], axis=0, inplace=True)
 print("UFOs shape: ", ufos.shape)
-print("UFOs data:\n", ufos.head(n=3))
+print("UFOs data:\n", ufos.sort_values('Location').head(n=3))
