f4d87368e83e4bc9d565c83f97939971e487f42f
objektwerks <objektwerks@runbox.com>
2017-11-24 07:33:25 -0500

    added index_col=0 to read_table

--- pandas.data.py
+++ pandas.data.py
@@ -3,11 +3,11 @@ Pandas to read features.
 """
 import pandas as panda
 
-orders = panda.read_table('./data/chipolte.tsv')
+orders = panda.read_table('./data/chipolte.tsv', index_col=0)
 print("Orders shape: ", orders.shape)
 print("Orders data:\n", orders.head(n=3))
 
 occupation_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
-occupations = panda.read_table('./data/occupations.psv', sep='|', header=None, names=occupation_cols)
+occupations = panda.read_table('./data/occupations.psv', sep='|', header=None, names=occupation_cols, index_col=0)
 print("Occupations shape: ", occupations.shape)
 print("Occupations data:\n", occupations.head(n=3))
