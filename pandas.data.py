22f3b0d88f8ddb2e2f16e83779e3aafa793e254d
objektwerks <objektwerks@runbox.com>
2017-11-24 07:28:18 -0500

    added occupations tsv and read table

--- pandas.data.py
+++ pandas.data.py
@@ -1,8 +1,13 @@
 """
-Using Pandas to read data files.
+Pandas to read features.
 """
 import pandas as panda
 
-dataframe = panda.read_table('./data/chipolte.tsv')
-print("Data shape: ", dataframe.shape)
-print("Data:\n", dataframe.head(n=3))
+orders = panda.read_table('./data/chipolte.tsv')
+print("Orders shape: ", orders.shape)
+print("Orders data:\n", orders.head(n=3))
+
+occupation_cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
+occupations = panda.read_table('./data/occupations.psv', sep='|', header=None, names=occupation_cols)
+print("Occupations shape: ", occupations.shape)
+print("Occupations data:\n", occupations.head(n=3))
