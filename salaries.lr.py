cbf7866b04b17fd2daf4afedd4b6f061242ad331
objektwerks <objektwerks@runbox.com>
2017-12-01 08:08:20 -0500

    refactored salaries LR model

--- salaries.lr.py
+++ salaries.lr.py
@@ -9,9 +9,6 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('./data/salaries.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
-print("X: ", X)
-print("y: ", y)
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 print("X shape / y shape: ", X.shape, y.shape)
 print("X train / X test shape: ", X_train.shape, X_test.shape)
@@ -19,8 +16,6 @@ print("y train / y test shape: ", y_train.shape, y_test.shape)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
-y_predicted = model.predict(X_test)
-print("y predicted: ", y_predicted)
 
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, model.predict(X_train), color = 'blue')
