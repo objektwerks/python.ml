5561806187f0650099992f408577c30ab2ee4aea
objektwerks <objektwerks@runbox.com>
2017-12-01 04:50:09 -0500

    added salaries.csv and LR test

---
+++ salaries.lr.py
@@ -0,0 +1,35 @@
+"""
+Linear Regression test on salaries data.
+"""
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('./data/salaries.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+print("X shape / y shape: ", X.shape, y.shape)
+print("X train / X test shape: ", X_train.shape, X_test.shape)
+print("y train / y test shape: ", y_train.shape, y_test.shape)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_predicted = model.predict(X_test)
+print("Accuracy score: ", metrics.accuracy_score(y_test, y_predicted))
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, model.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, model.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
