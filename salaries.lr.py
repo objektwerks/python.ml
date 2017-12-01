bdfb22f9339af83a85830f589e2613a597334443
objektwerks <objektwerks@runbox.com>
2017-12-01 05:15:13 -0500

    implemented salaries LR model

--- salaries.lr.py
+++ salaries.lr.py
@@ -2,15 +2,17 @@
 Linear Regression test on salaries data.
 """
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('./data/salaries.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+print("X: ", X)
+print("y: ", y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 print("X shape / y shape: ", X.shape, y.shape)
 print("X train / X test shape: ", X_train.shape, X_test.shape)
 print("y train / y test shape: ", y_train.shape, y_test.shape)
@@ -18,7 +20,7 @@ print("y train / y test shape: ", y_train.shape, y_test.shape)
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_predicted = model.predict(X_test)
-print("Accuracy score: ", metrics.accuracy_score(y_test, y_predicted))
+print("y predicted: ", y_predicted)
 
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, model.predict(X_train), color = 'blue')
