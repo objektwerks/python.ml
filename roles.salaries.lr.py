66a3f0d566dc1eafc281fa9f3ca352310a9180dc
objektwerks <objektwerks@runbox.com>
2017-12-04 08:37:16 -0500

    removed comments

--- roles.salaries.lr.py
+++ roles.salaries.lr.py
@@ -21,7 +21,6 @@ features.fit(X_poly, y)
 ploy_model = LinearRegression()
 ploy_model.fit(X_poly, y)
 
-# Linear Regression plot.
 plt.scatter(X, y, color = 'red')
 plt.plot(X, model.predict(X), color = 'blue')
 plt.title('Linear Regression')
@@ -29,7 +28,6 @@ plt.xlabel('Role-Level')
 plt.ylabel('Salary')
 plt.show()
 
-# Linear Polynomial Regression plot.
 plt.scatter(X, y, color = 'red')
 plt.plot(X, ploy_model.predict(features.fit_transform(X)), color = 'blue')
 plt.title('Linear Polynomial Regression')
@@ -37,7 +35,6 @@ plt.xlabel('Role-Level')
 plt.ylabel('Salary')
 plt.show()
 
-# Linear Polynomial Regression smooth plot.
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
@@ -47,8 +44,5 @@ plt.xlabel('Role-Level')
 plt.ylabel('Salary')
 plt.show()
 
-# Linear Regression
 model.predict(6.5)
-
-# Linear Polynomial Regression
 ploy_model.predict(features.fit_transform(6.5))
