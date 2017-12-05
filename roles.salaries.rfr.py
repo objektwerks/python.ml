2a396ac5a66bd60e687d875f011db5dbdc7df62d
objektwerks <objektwerks@runbox.com>
2017-12-05 07:12:19 -0500

    refactored dtr and rfr models

--- roles.salaries.rfr.py
+++ roles.salaries.rfr.py
@@ -1,41 +1,29 @@
-# Random Forest Regression
-
-# Importing the libraries
+"""
+Random Forest Regression test on roles.salaries data.
+"""
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+df = pd.read_csv('./data/roles.salaries.csv')
+X = df.iloc[:, 1:2].values
+y = df.iloc[:, 2].values
+print("X shape / y shape: ", X.shape, y.shape)
+print("X:\n", X)
+print("y:\n", y)
 
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+model = RandomForestRegressor(n_estimators = 10, random_state = 0)
+model.fit(X, y)
+y_predicted = model.predict(6.5)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
-# Fitting Random Forest Regression to the dataset
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
-
-# Predicting a new result
-y_pred = regressor.predict(6.5)
-
-# Visualising the Random Forest Regression results (higher resolution)
+# Role-Level 1 - 10, step 0.1
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
+
 plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
+plt.plot(X_grid, model.predict(X_grid), color = 'blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Role-Level')
 plt.ylabel('Salary')
 plt.show()
