2b245e424da90b73ae998116c6506bb74b3fa286
objektwerks <objektwerks@runbox.com>
2018-01-14 12:52:39 -0500

    renamed scripts

--- ml/classification/social.network.ads.rbf.svm.py
+++ ml/classification/social.network.ads.rbf.svm.py
@@ -1,5 +1,5 @@
 """
-Support Vector Machine test on social network ads data.
+Gaussian RBF Support Vector Machine test on social network ads data.
 """
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -22,7 +22,7 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-model = SVC(kernel = 'linear', random_state = 0)
+model = SVC(kernel = 'rbf', random_state = 0)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 confusion_matrix = confusion_matrix(y_test, y_pred)
@@ -38,7 +38,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Training set)')
+plt.title('Kernal SVM (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -54,7 +54,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('SVM (Test set)')
+plt.title('Kernal SVM (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
