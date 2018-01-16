87c5d11c1f3cf05c72d19c274a5bc1fb04516dcf
objektwerks <objektwerks@runbox.com>
2018-01-16 09:39:15 -0500

    edited clustering scripts

--- ml/clustering/mall.customers.kmeans.py
+++ ml/clustering/mall.customers.kmeans.py
@@ -2,25 +2,12 @@
 K-Means clustering test on mall customer data.
 """
 import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./../../data/mall.customers.csv')
 X = df.iloc[:, [3, 4]].values
-"""
-y = df.iloc[:, 3].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-"""
-
-"""
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
-"""
+print("X shape: ", X.shape)
 
 wcss = []
 for i in range(1, 11):
