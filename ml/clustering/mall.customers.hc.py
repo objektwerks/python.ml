b3eceec32b4442475588ab8ed3e05fde15e84ce6
objektwerks <objektwerks@runbox.com>
2018-01-16 09:27:07 -0500

    edited clustering scripts

--- ml/clustering/mall.customers.hc.py
+++ ml/clustering/mall.customers.hc.py
@@ -2,16 +2,16 @@
 Hierarchical clustering test on mall customer data.
 """
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
 import scipy.cluster.hierarchy as sch
 from sklearn.cluster import AgglomerativeClustering
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./../../data/mall.customers.csv')
 X = df.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
 """
+y = df.iloc[:, 3].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 """
 
@@ -37,8 +37,8 @@ plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Clus
 plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
 plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
 plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
+plt.title('Clusters of Customers')
+plt.xlabel('Annual Income')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
--- ml/clustering/mall.customers.kmeans.py
+++ ml/clustering/mall.customers.kmeans.py
@@ -2,15 +2,15 @@
 K-Means clustering test on mall customer data.
 """
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('./../../data/mall.customers.csv')
 X = df.iloc[:, [3, 4]].values
-# y = dataset.iloc[:, 3].values
 """
+y = df.iloc[:, 3].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 """
 
@@ -42,8 +42,8 @@ plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', labe
 plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
 plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
+plt.title('Clusters of Customers')
+plt.xlabel('Annual Income')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
