c66738b2ce52c79245a494cf59d6d1e38e419b65
objektwerks <objektwerks@runbox.com>
2018-01-17 09:41:18 -0500

    edited shopping apriori script

--- ml/apriori/shopping.apriori.py
+++ ml/apriori/shopping.apriori.py
@@ -8,6 +8,7 @@ from apyori import apriori
 df = pd.read_csv('./../../data/shopping.csv', header = None)
 transactions = []
 for i in range(0, 7501):
-    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    transactions.append([str(df.values[i,j]) for j in range(0, 20)])
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 results = list(rules)
+print("Results: ", results)
