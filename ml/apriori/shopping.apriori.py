e424f23718e37c5ba1cde67ebb8b39deac775855
objektwerks <objektwerks@runbox.com>
2018-01-18 07:04:52 -0500

    added debug to shopping script

--- ml/apriori/shopping.apriori.py
+++ ml/apriori/shopping.apriori.py
@@ -5,10 +5,10 @@ import pandas as pd
 from apyori import apriori
 
 df = pd.read_csv('./../../data/shopping.csv', header = None)
-transactions = []
+baskets = []
 for i in range(0, 7501):
-    transactions.append([str(df.values[i, j]) for j in range(0, 20)])
-records = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+    baskets.append([str(df.values[i, j]) for j in range(0, 20)])
+records = apriori(baskets, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 j = 0
 for record in records:
     print(record)
