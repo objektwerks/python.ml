07e9cfeeef5f075c7fcb4bd66dc54f43f95f4c64
objektwerks <objektwerks@runbox.com>
2018-01-18 07:30:31 -0500

    edited shopping py script

--- ml/apriori/shopping.apriori.py
+++ ml/apriori/shopping.apriori.py
@@ -8,10 +8,10 @@ df = pd.read_csv('./../../data/shopping.csv', header = None)
 baskets = []
 for i in range(0, 7501):
     baskets.append([str(df.values[i, j]) for j in range(0, 20)])
-records = apriori(baskets, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+relations = apriori(baskets, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 j = 0
-for record in records:
-    print(record)
+for relation in relations:
+    print(relation)
     j += 1
     if j == 10:
         break
