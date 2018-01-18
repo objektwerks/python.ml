79f29161c4555aba3943ea312b46e81d2a1f9065
objektwerks <objektwerks@runbox.com>
2018-01-18 06:53:20 -0500

    edited apriori py script

--- ml/apriori/shopping.apriori.py
+++ ml/apriori/shopping.apriori.py
@@ -9,4 +9,9 @@ transactions = []
 for i in range(0, 7501):
     transactions.append([str(df.values[i, j]) for j in range(0, 20)])
 records = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
-print("Results:\n", list(records))
+j = 0
+for record in records:
+    print(record)
+    j += 1
+    if j == 10:
+        break
