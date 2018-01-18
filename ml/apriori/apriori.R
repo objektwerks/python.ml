d90e8a72f1288550c358e9f19d232cfec41f52af
objektwerks <objektwerks@runbox.com>
2018-01-18 07:34:17 -0500

    edited apriori.R and eclast.R scripts

--- ml/apriori/apriori.R
+++ ml/apriori/apriori.R
@@ -2,10 +2,10 @@
 
 install.packages('arules')
 library(arules)
+
 dataset = read.csv('./../../data/shopping.csv', header = FALSE)
 dataset = read.transactions('./../../data/shopping.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
-
 rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 inspect(sort(rules, by = 'lift')[1:10])
--- ml/apriori/eclat.R
+++ ml/apriori/eclat.R
@@ -2,10 +2,10 @@
 
 install.packages('arules')
 library(arules)
+
 dataset = read.csv('./../../data/shopping.csv')
 dataset = read.transactions('./../../data/shopping.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
-
 rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
 inspect(sort(rules, by = 'support')[1:10])
