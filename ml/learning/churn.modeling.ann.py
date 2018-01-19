dbbc625e32df4cdde18ab465e9495bf6f4522feb
objektwerks <objektwerks@runbox.com>
2018-01-19 07:21:35 -0500

    edited churn modeling ann script format

--- ml/learning/churn.modeling.ann.py
+++ ml/learning/churn.modeling.ann.py
@@ -36,7 +36,6 @@ classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu'))
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100)
-
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 cm = confusion_matrix(y_test, y_pred)
