01772394f61981f9d9edaaec869128157540699e
objektwerks <objektwerks@runbox.com>
2018-01-19 05:33:23 -0500

    added nlp data and script

---
+++ ml/nlp/restaurant.reviews.nlp.py
@@ -0,0 +1,39 @@
+"""
+Natural Language Processing test on restaurant review data.
+"""
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('./../../data/restaurant.reviews.tsv', delimiter = '\t', quoting = 3)
+
+nltk.download('stopwords')
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', df['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+model = CountVectorizer(max_features = 1500)
+X = model.fit_transform(corpus).toarray()
+y = df.iloc[:, 1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+print("X shape / y shape: ", X.shape, y.shape)
+print("X train / X test shape: ", X_train.shape, X_test.shape)
+print("y train / y test shape: ", y_train.shape, y_test.shape)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: ", cm)
