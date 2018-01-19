dfa0648c4dd3b55344e222fb4027c8ffc898192c
objektwerks <objektwerks@runbox.com>
2018-01-19 07:02:01 -0500

    edited neural networkscripts

--- ml/learning/cats.dogs.cnn.py
+++ ml/learning/cats.dogs.cnn.py
@@ -1,49 +1,26 @@
-# Convolutional Neural Network
+"""
+Convolutional Neural Network test on cats and dogs data.
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
-
-# Importing the Keras libraries and packages
+WARNING: The cats and dogs dataset is TOO LARGE to push to Github. Download at
+https://www.superdatascience.com/machine-learning/ , Part 8. Deep Learning,
+Convolutional Neural Networks.zip
+"""
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
 
-# Initialising the CNN
 classifier = Sequential()
-
-# Step 1 - Convolution
 classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
-
-# Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Adding a second convolutional layer
 classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-# Step 3 - Flattening
 classifier.add(Flatten())
-
-# Step 4 - Full connection
 classifier.add(Dense(output_dim = 128, activation = 'relu'))
 classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
-
-# Compiling the CNN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# Part 2 - Fitting the CNN to the images
-
-from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
@@ -52,12 +29,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('./../../dataset/training',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('./../../dataset/test',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
