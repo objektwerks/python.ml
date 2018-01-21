922fae67bc26f4e864b556d32c15e1fcce3a2fe8
objektwerks <objektwerks@runbox.com>
2018-01-21 15:21:26 -0500

    edited cats dogs cnn script

--- dl/cats.dogs.cnn.py
+++ dl/cats.dogs.cnn.py
@@ -61,14 +61,12 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
-
-test_datagen = ImageDataGenerator(rescale = 1./255)
-
 training_set = train_datagen.flow_from_directory('./../dataset/training',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
+test_datagen = ImageDataGenerator(rescale = 1./255)
 test_set = test_datagen.flow_from_directory('./../dataset/test',
                                             target_size = (64, 64),
                                             batch_size = 32,
