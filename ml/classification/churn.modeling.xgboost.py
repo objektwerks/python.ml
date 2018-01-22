134c486ff656e845d2a6bff857f0d9ce6a2b41c6
objektwerks <objektwerks@runbox.com>
2018-01-22 06:18:04 -0500

    edited dimensionality reduction scripts

--- ml/classification/churn.modeling.xgboost.py
+++ ml/classification/churn.modeling.xgboost.py
@@ -1,19 +1,26 @@
-# XGBoost
+"""
+XGBoost test on churn modeling data.
 
-# Install xgboost following the instructions on this link: http://xgboost.readthedocs.io/en/latest/build.html#
+INSTALL:
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
+1. brew install gcc
+2. git clone --recursive https://github.com/dmlc/xgboost
+3. cd xgboost
+4. ./build.sh
+5. cd python-package
+6. python3 setup.py install
+"""
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
-
-# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv('./../../data/churn.modeling.csv')
+X = df.iloc[:, 3:13].values
+y = df.iloc[:, 13].values
+
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
@@ -21,25 +28,17 @@ X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+print("X shape / y shape: ", X.shape, y.shape)
+print("X train / X test shape: ", X_train.shape, X_test.shape)
+print("y train / y test shape: ", y_train.shape, y_test.shape)
 
-# Fitting XGBoost to the Training set
-from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: ", cm)
 
-# Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-accuracies.mean()
-accuracies.std()
+print("Accuracy mean: ", accuracies.mean())
+print("Accuracy std: ", accuracies.std())
--- ml/classification/social.network.ads.grid.search.py
+++ ml/classification/social.network.ads.grid.search.py
@@ -1,48 +1,42 @@
-# Grid Search
-
-# Importing the libraries
+"""
+Grid Search test on social network ads.
+"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+df = pd.read_csv('./../../data/social.network.ads.csv')
+X = df.iloc[:, [2, 3]].values
+y = df.iloc[:, 4].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+print("X shape / y shape: ", X.shape, y.shape)
+print("X train / X test shape: ", X_train.shape, X_test.shape)
+print("y train / y test shape: ", y_train.shape, y_test.shape)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Kernel SVM to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+model = SVC(kernel = 'rbf', random_state = 0)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: ", cm)
 
-# Applying k-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-accuracies.mean()
-accuracies.std()
+accuracies = cross_val_score(estimator = model, X = X_train, y = y_train, cv = 10)
+print("Accuracy mean: ", accuracies.mean())
+print("Accuracy std: ", accuracies.std())
 
-# Applying Grid Search to find the best model and the best parameters
-from sklearn.model_selection import GridSearchCV
 parameters = [{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
               {'C': [1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
-grid_search = GridSearchCV(estimator = classifier,
+grid_search = GridSearchCV(estimator = model,
                            param_grid = parameters,
                            scoring = 'accuracy',
                            cv = 10,
@@ -50,13 +44,13 @@ grid_search = GridSearchCV(estimator = classifier,
 grid_search = grid_search.fit(X_train, y_train)
 best_accuracy = grid_search.best_score_
 best_parameters = grid_search.best_params_
+print("Best Accuracy: ", best_accuracy)
+print("Best Parameters: ", best_parameters)
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
@@ -69,12 +63,10 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
