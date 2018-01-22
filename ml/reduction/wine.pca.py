42447ebf03960c1b04ca4e5ac99505687d51ca9a
objektwerks <objektwerks@runbox.com>
2018-01-22 06:31:01 -0500

    edited dimensionality reduction scripts

--- ml/reduction/wine.pca.py
+++ ml/reduction/wine.pca.py
@@ -1,46 +1,40 @@
-# PCA
-
-# Importing the libraries
+"""
+PCA test on wine data.
+"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Wine.csv')
-X = dataset.iloc[:, 0:13].values
-y = dataset.iloc[:, 13].values
-
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+df = pd.read_csv('./../../data/wine.csv')
+X = df.iloc[:, 0:13].values
+y = df.iloc[:, 13].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+print("X shape / y shape: ", X.shape, y.shape)
+print("X train / X test shape: ", X_train.shape, X_test.shape)
+print("y train / y test shape: ", y_train.shape, y_test.shape)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Applying PCA
-from sklearn.decomposition import PCA
-pca = PCA(n_components = 2)
-X_train = pca.fit_transform(X_train)
-X_test = pca.transform(X_test)
-explained_variance = pca.explained_variance_ratio_
+model = PCA(n_components = 2)
+X_train = model.fit_transform(X_train)
+X_test = model.transform(X_test)
+explained_variance = model.explained_variance_ratio_
+print("PCA Explained Variance Ratio: ", explained_variance)
 
-# Fitting Logistic Regression to the Training set
-from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print("Confusion matrix: ", cm)
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
@@ -57,8 +51,6 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
