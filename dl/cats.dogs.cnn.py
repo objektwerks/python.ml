a2f428a9f758292767ade355b418e6dd6b79ad57
objektwerks <objektwerks@runbox.com>
2018-01-24 15:18:10 -0500

    reformatted cnn script code

--- dl/cats.dogs.cnn.py
+++ dl/cats.dogs.cnn.py
@@ -47,16 +47,6 @@ from keras.layers import Flatten
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
 
-classifier = Sequential()
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = "relu"))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Conv2D(32, (3, 3), activation = "relu"))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-classifier.add(Flatten())
-classifier.add(Dense(activation = "relu", units = 128))
-classifier.add(Dense(activation = "sigmoid", units = 1))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
@@ -71,7 +61,16 @@ test_set = test_datagen.flow_from_directory('./../dataset/test',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
-
+                                            
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = "relu"))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation = "relu"))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+classifier.add(Dense(activation = "relu", units = 128))
+classifier.add(Dense(activation = "sigmoid", units = 1))
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 classifier.fit_generator(training_set,
                          steps_per_epoch = 250,
                          epochs = 10,
