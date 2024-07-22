ab37f614a4c6c29bab36bd11da4550f95744db96
objektwerks <objektwerks@runbox.com>
2024-07-22 13:34:30 -0400

    edited file path

--- src/ml/classification/naive.bayes.py
+++ src/ml/classification/naive.bayes.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('./../../data/social.network.ads.csv')
+df = pd.read_csv('./../../../data/social.network.ads.csv')
 X = df.iloc[:, [2, 3]].values
 y = df.iloc[:, 4].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
