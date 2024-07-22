e444585d6d647e27bf398ec7264968fdc52bc459
objektwerks <objektwerks@runbox.com>
2024-07-22 13:26:19 -0400

    edited file path

--- src/ml/learning/apriori.R
+++ src/ml/learning/apriori.R
@@ -3,8 +3,8 @@
 install.packages('arules')
 library(arules)
 
-dataset = read.csv('./../../data/shopping.csv', header = FALSE)
-dataset = read.transactions('./../../data/shopping.csv', sep = ',', rm.duplicates = TRUE)
+dataset = read.csv('./../../../data/shopping.csv', header = FALSE)
+dataset = read.transactions('./../../../data/shopping.csv', sep = ',', rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
