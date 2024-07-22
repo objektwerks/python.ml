976824ca437aba15cc097b4f226ce37e7637e1b0
objektwerks <objektwerks@runbox.com>
2024-07-22 13:12:05 -0400

    edited xgboost.py

--- src/ml/classification/xgboost.py
+++ src/ml/classification/xgboost.py
@@ -25,7 +25,7 @@ labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features=[1])
+onehotencoder = OneHotEncoder()
 X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
