f7da87c6e2eb32156274dbc6b6c0bdd42c14b68a
objektwerks <objektwerks@runbox.com>
2024-07-22 13:28:43 -0400

    edited file path

--- src/ml/learning/random.selection.py
+++ src/ml/learning/random.selection.py
@@ -13,7 +13,7 @@ d = 10
 ads_selected = []
 total_reward = 0
 for n in range(0, N):
-    ad = random.randrange(d
+    ad = random.randrange(d)
     ads_selected.append(ad)
     reward = df.values[n, ad]
     total_reward = total_reward + reward
